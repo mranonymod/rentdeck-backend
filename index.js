5061cd08164a305e986760f53e3bf94b37036666
mranonymod <64246578+mranonymod@users.noreply.github.com>
2021-10-08 21:40:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const dotenv=require("dotenv")
 dotenv.config();
 
 const app = express();
+app.use(cors())
 app.use(express.json())
 app.use(session({secret:'SOME'}))
 app.use(passport.initialize())
