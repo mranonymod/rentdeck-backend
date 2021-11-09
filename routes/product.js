2dcfa2b4a165f5dd5117fdafd708918743fb170e
mranonymod <mryash018@gmail.com>
2021-11-10 00:33:36 +0530

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
-const { getProducts , getProductById , searchProductbyTitle , getTopProducts , getByCategories} = require('../controllers/productsC.js')
+const { getProducts , getProductById , searchProductbyTitle , getTopProducts , getByCategories,someProducts} = require('../controllers/productsC.js')
 //GET ALL PROUDUCTS
 router.route('/').get(getProducts)
+
+router.route('/some?:n').get(someProducts)
 
 router.route('/s?:q').get(searchProductbyTitle)
 
