0e401610113c75f258291670cee13e396edd148d
mranonymod <mryash018@gmail.com>
2021-11-09 20:46:38 +0530

    Create authMiddleware.js

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,41 @@
+const jwt = require('jsonwebtoken')
+const asyncHandler=require('express-async-handler')
+const User =require('../models/User')
+
+const tkCheck = asyncHandler(async (req, res, next) => {
+  let token
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      token = req.headers.authorization.split(' ')[1]
+
+      const decoded = jwt.verify(token, process.env.JWT_KEY)
+
+      req.user = await User.findById(decoded.id).select('-password')
+      console.log("jwt"+ req.user)
+      next()
+    } catch (error) {
+      console.error(error)
+      res.status(401)
+      throw new Error('Not authorized, token failed')
+    }
+  }
+  if (!token) {
+    res.status(401)
+    throw new Error('Not authorized, no token')
+  }
+})
+
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next()
+  } else {
+    res.status(401)
+    throw new Error('Not authorized as an admin')
+  }
+}
+
+module.exports= { tkCheck, admin }
